1d0baa23f2761091341e61e07bf62c86c80616d5
hybrid2k3 <hybr@mail.ru>
2017-12-14 11:33:25 +0300

    reworked

--- json_parse.py
+++ json_parse.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     filepath = sys.argv[1]
     if os.path.isfile(filepath):
         with open(filepath, encoding='utf-8') as json_file:
-            json_parsed = json.load(json_file)
-            pprint.pprint(json_parsed)
+            data = json.load(json_file)
+            pprint.pprint(data)
     else:
         print('Такого файла не существует')
