57c587e3eaa2cbdc1136ab93a774cd697c9737ff
hybrid2k3 <hybr@mail.ru>
2017-12-14 10:53:04 +0300

    reworked script(add functions and pprint)

--- json_parse.py
+++ json_parse.py
@@ -1,13 +1,25 @@
 import json
 import os
+import pprint
 import sys
 
-filename = sys.argv[1]
-if os.path.isfile(filename):
-    with open(filename,encoding='utf-8'.format(filename)) as json_file:
-        parsed = json.load(json_file)
-        print(json.dumps(parsed, indent=4,ensure_ascii=False, sort_keys=True))
-else:
-    print('Такого файла не существует')
+
+def load_data(filepath):
+    if os.path.isfile(filepath):
+        with open(filepath, encoding='utf-8') as json_file:
+            data = json.load(json_file)
+            return data
+
+def pretty_print_json(data):
+    pprint.pprint(data)
 
 
+if __name__ == '__main__':
+    
+    filepath = sys.argv[1]
+    if os.path.isfile(filepath):
+        with open(filepath, encoding='utf-8') as json_file:
+            json_parsed = json.load(json_file)
+            pprint.pprint(json_parsed)
+    else:
+        print('Такого файла не существует')
